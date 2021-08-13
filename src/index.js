938ef36e34fa3c9ecedea2e3b8a4cda6850bfd90
Robert King <robert.o.king@hotmail.co.uk>
2021-08-13 19:17:40 +0200

    Added: Wiring for outputUser <iframe>

--- src/index.js
+++ src/index.js
@@ -8,8 +8,39 @@ initResizeable();
 
 const inputTextarea = document.getElementById('input-textarea');
 const inputCode = document.getElementById('input-code');
+const outputUser = document.getElementById('output-user');
+
+const html = '<p>Test!</p>';
+
+/* https://stackoverflow.com/a/61421435 */
+const updateOutputUserDoc = (css) => {
+  const iframeHTML = `
+    <!doctype html>
+    <html>
+      <head>
+        <style>${css}</style>
+      </head>
+      <body>${html}</body>
+    </html>`;
+  outputUser.src = 'data:text/html,' + encodeURIComponent(iframeHTML);
+};
+
 
 inputTextarea.addEventListener('input', (event) => {
   const codeToHighlight = inputTextarea.value;
   inputCode.innerHTML = Prism.highlight(codeToHighlight, Prism.languages.css, 'css');
+  updateOutputUserDoc(codeToHighlight);
 });
+
+const mapCharsToEscape = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#039;'
+};
+
+/* https://stackoverflow.com/a/4835406 */
+const escapeHtml = (htmlString) => {
+  return htmlString.replace(/[&<>"']/g, function(m) { return mapCharsToEscape[m]; });
+};
