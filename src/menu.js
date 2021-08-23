f2d2f7e65a343eeb53c6f20101b0cad92b87b198
Robert King <robert.o.king@hotmail.co.uk>
2021-08-23 17:00:03 +0200

    Added: populateMenu() async function which currently only handles failures

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,23 @@
-const populateMenu = async () => {
-  const data = await fetch('http://localhost:3000/challenges/1')
-      .then(response => response.json());
+const loading = document.getElementById('loading');
+const loadError = document.getElementById('load-error');
+const challengeForm = document.getElementById('challenge-form');
 
-  console.log(data);
+const onLoadFail = () => {
+  loading.classList.remove('display-content');
+  loadError.classList.add('display-content');
+};
+
+const populateMenu = async () => {
+  try {
+    const data = await fetch('http://localhost:3000/challenges/1')
+        .then(response => response.json());
+
+    if (!response.ok) {
+      onLoadFail();
+    }
+  } catch(e) {
+    onLoadFail();
+  }
 };
 
 export const initMenu = () => {
