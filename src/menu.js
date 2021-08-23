9e24bafe95a06f33b54c1d19f54e6dc7c58cb2da
Robert King <robert.o.king@hotmail.co.uk>
2021-08-23 17:22:08 +0200

    Added: async function loadChallenges()

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,36 @@
 const loading = document.getElementById('loading');
 const loadError = document.getElementById('load-error');
-const challengeForm = document.getElementById('challenge-form');
+const challengeSelect = document.getElementById('challenge-select');
+
+const loadChallenges = async () => {
+  const response = await fetch('http://localhost:3000/challenges');
+  const data = await response.json();
+
+  return data;
+};
 
 const onLoadFail = () => {
   loading.classList.remove('display-content');
   loadError.classList.add('display-content');
 };
 
-const populateMenu = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/challenges/1');
+const buildMenu = (data) => {
+  data.forEach((challenge) => {
+    challengeSelect.insertAdjacentHTML('beforeend', `<option value=${challenge.id}>${challenge.name}</option>`);
+  })
+};
 
-    if (!response.ok) {
-      onLoadFail();
-    }
+const populateMenu = async () => {
+  let data = [];
+
+  try {
+    data = await loadChallenges();
+    // buildMenu(data.json());
   } catch(e) {
     onLoadFail();
   }
+
+  console.log(data);
 };
 
 export const initMenu = () => {
